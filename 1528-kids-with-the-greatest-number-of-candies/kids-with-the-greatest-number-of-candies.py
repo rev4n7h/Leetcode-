bbbeea1d4412b00049b5e3a0dd192c0c6b5962b3
Revanthreddy <challa.2024ug2009@iiitranchi.ac.in>
2026-09-06 23:36:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (57.09%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -1,8 +1,9 @@
 class Solution(object):
     def kidsWithCandies(self, candies, extraCandies):
         arr = []
+        mc = max(candies)
         for i in range(len(candies)):
-            if candies[i] + extraCandies >= max(candies):
+            if candies[i] + extraCandies >= mc:
                arr.insert(i,True)
             
             else :
